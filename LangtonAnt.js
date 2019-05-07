de615287baa13cfb6538a19b30bd2c21bd29ae63
Will McAuliff <womcauliff@gmail.com>
2019-05-07 18:04:27 -0400

    Step 1
    Writes tests for step 1 of Langton Ant simulation. Implements passing code. Reorganizes state.

--- LangtonAnt.js
+++ LangtonAnt.js
@@ -1,15 +1,29 @@
 class LangtonAnt {
-  grid() {
-    return [
-      [
+  constructor() {
+    this.step = 0;
+    this.grid = [
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1]
+    ];
+    this.ant = { x: 2, y: 2, dir: "⬅️" };
+  }
+
+  next() {
+    if (this.step === 1) {
+      this.grid = [
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1],
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1]
-      ],
-      { x: 2, y: 2, dir: "⬅️" }
-    ];
+      ];
+      this.ant = { x: 2, y: 1, dir: "⬆️" };
+    }
+    this.step += 1;
+    return [this.grid, this.ant];
   }
 }
 
--- LangtonAnt.test.js
+++ LangtonAnt.test.js
@@ -4,8 +4,8 @@ describe(`LangtonAnt`, () => {
   const langtonAnt = new LangtonAnt(5);
 
   describe(`initial grid`, () => {
-    const [initialGrid, antLocation] = langtonAnt.grid();
-    it(`should be 5x5 grid of white tiles`, () => {
+    const [initialGrid, antLocation] = langtonAnt.next();
+    it(`grid should be 5x5 white tiles`, () => {
       expect(initialGrid).toEqual([
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
@@ -14,11 +14,23 @@ describe(`LangtonAnt`, () => {
         [1, 1, 1, 1, 1]
       ]);
     });
-    it(`ant should be in center tile, facing left`, () => {
+    it(`ant should be on center tile, facing left`, () => {
       expect(antLocation).toEqual({ x: 2, y: 2, dir: "⬅️" });
     });
   });
-  // describe(`step 1`, () => {
-  //   it(`should have ant take step to right`, () => {});
-  // });
+  describe(`step 1`, () => {
+    const [grid, ant] = langtonAnt.next();
+    it(`center tile flips to black`, () => {
+      expect(grid).toEqual([
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
+      ]);
+    });
+    it(`ant rotates 90 deg, moves up one tile`, () => {
+      expect(ant).toEqual({ x: 2, y: 1, dir: "⬆️" });
+    });
+  });
 });
